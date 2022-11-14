741e1fee1d254c8af555c2096a7e991e135a6841
 <arsauravsingh@gmail.com>
2022-11-14 15:07:25 +0530

    Author model created

--- src/models/authorModel.js
+++ src/models/authorModel.js
@@ -1,1 +1,28 @@
-const mongoose = require('mongoose')
+const mongoose = required('mongoose')
+
+const authorSchema= new mongoose.Schema({
+    fname:{
+        type:String,
+        required:true
+    },
+    lname:{
+        type:String,
+        required:true
+    },
+    title:{
+        type:String,
+        required:true,
+        enum:["Mr", "Mrs", "Miss"]
+    },
+
+    email: {
+        type: mongoose.SchemaTypes.Email, 
+        required: true,
+        unique:true  
+    },
+    password:{
+        type: String,
+        required:true
+    }
+},{timestamps:true})
+module.exports = mongoose.model("Author", authorSchema)

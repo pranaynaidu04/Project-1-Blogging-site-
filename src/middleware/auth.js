f1105db2961db73dd5c944e09fd5fc47502848c5
SiddhantYerandkar <siddhantyerandkar@gmail.com>
2022-11-16 15:57:23 +0530

    authorise created

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,4 +1,5 @@
 const jwt = require("jsonwebtoken");
+const blogModel = require("../models/blogModel");
 
 const authenticate = function (req, res, next) {
   try {
@@ -10,12 +11,30 @@ const authenticate = function (req, res, next) {
     if (!decodedToken) {
       return res.status(401).send({ msg: "token is invalid" });
     }
-    req.authorLoggedIn = decodedToken.authorId
+    req.authorLoggedIn = decodedToken.authorId;
+    next();
+  } catch (error) {
+    return res.status(500).send({ status: false, msg: error.message });
+  }
+};
+
+const authorise = async function (req, res, next) {
+  try {
+    let blogId = req.params.blogId
+    
+    let blogs = await blogModel.findById(blogId)
+    let authorId = blogs.authorId
+    if(!authorId){ 
+      authorId=req.query.authorId
+    }
+    if (authorId != req.authorLoggedIn) {
+      return res.status(403).send({ msg: "you dont have access" });
+    }
     next();
   } catch (error) {
     return res.status(500).send({status:false, msg: error.message})
   }
 };
 
-
 module.exports.authenticate = authenticate;
+module.exports.authorise = authorise;
--- src/routes/route.js
+++ src/routes/route.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const authorController = require("../controller/authorController");
 const blogController = require("../controller/blogController");
-const authenticate = require("../middleware/auth")
+const auth = require("../middleware/auth")
 
 router.get("/test", function (req, res) {
   res.send({ data: "test api" });
@@ -12,19 +12,19 @@ router.get("/test", function (req, res) {
 router.post("/authors", authorController.createAuthor);
 
 //blog creation
-router.post("/blogs", authenticate.authenticate, blogController.createBlog);
+router.post("/blogs", auth.authenticate, blogController.createBlog);
 
 //fetch blogs
-router.get("/blogs",authenticate.authenticate, blogController.getBlogs);
+router.get("/blogs",auth.authenticate, blogController.getBlogs);
 
 //update blogs
-router.put("/blogs/:blogId",authenticate.authenticate, blogController.updateBlogs);
+router.put("/blogs/:blogId",auth.authenticate, auth.authorise , blogController.updateBlogs);
 
 //delete blogs
-router.delete("/blogs/:blogId",authenticate.authenticate, blogController.deleteBlog);
+router.delete("/blogs/:blogId",auth.authenticate, auth.authorise, blogController.deleteBlog);
 
 // delete blogs using query
-router.delete("/blogs",authenticate.authenticate, blogController.deleteBlogByQuery)
+router.delete("/blogs",auth.authenticate, auth.authorise, blogController.deleteBlogByQuery)
 
 //user login 
 router.post("/login",authorController.loginUser)
